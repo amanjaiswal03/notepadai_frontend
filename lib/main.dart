34bd3280109aee961f2b50e34042532bd48cecca
fbachus <felix.bachstein@gmail.com>
2019-02-21 18:33:41 +0100

    created a routes folder containing different screens, to keep the main file clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notepadai_app/routes/homescreen.dart';
 import 'dart:async';
 import 'package:notepadai_app/design elements/DiamondBorder.dart';
 import 'package:audioplayers/audioplayers.dart';
@@ -38,66 +39,3 @@ class NotepadAI extends StatelessWidget {
 }
 
 // Starting screen upon run
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-// Subclass of the HomePage specifying style and interactive elements and putting them in a grid
-class _HomePageState extends State<HomePage> {
-  static int _isPressed = 0;
-
-  /* Test purposes only: */
-  static final _floatingButtonState = [
-    Icon(Icons.keyboard_voice),
-    Icon(Icons.stop),
-  ];
-  static final _floatingButtonColor = [
-    MAIN_COLOR,
-    Colors.red,
-  ];
-
-  Icon _buttonIcon = _floatingButtonState[_isPressed];
-  Color _buttonColor = _floatingButtonColor[_isPressed];
-
-  void _streamAudio() {
-    setState(() {
-      _buttonIcon = _floatingButtonState[_isPressed];
-      _buttonColor = _floatingButtonColor[_isPressed];
-      _isPressed = (_isPressed + 1) % 2;
-    });
-
-    StreamController<String> controller = new StreamController();
-    //Stream<String> audioStream = new Stream.fromFuture();
-  }
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.list), title: Text('All Notes')),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite_border), title: Text('Favorites')),
-          BottomNavigationBarItem(icon: Icon(Icons.edit), title: Text('Editor')),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text('Settings'))
-        ],
-        type: BottomNavigationBarType.fixed,
-      ),
-      // BUTTON FOR TESTING PURPOSE ONLY - REMOVE OR CHANGE ONCE TEST COMPLETED (->AUDIO OVER gRPC)
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: _buttonColor,
-        child: _buttonIcon,
-        onPressed: _streamAudio,
-        tooltip: 'Start streaming audio',
-      ),
-    );
-  }
-}
