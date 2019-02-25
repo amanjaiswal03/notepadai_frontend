612b8001407c61cf2ef5f5bb62a44c995d17fed5
fbachus <felix.bachstein@gmail.com>
2019-02-25 13:51:12 +0100

    added floating button to FavPage

--- lib/routes/favorites.dart
+++ lib/routes/favorites.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:notepadai_app/main.dart';
 //import 'package:notepadai_app/design elements/DiamondBorder.dart';
+//import 'package:audioplayers/audioplayers.dart';
+//import 'package:audio_recorder/audio_recorder.dart';
+//import 'package:notepadai_app/proto/audioStream.pbgrpc.dart';
+//import 'package:notepadai_app/proto/audioStream.pb.dart';
+//import 'package:grpc/grpc.dart';
 
 class FavPage extends StatefulWidget {
   FavPage({Key key, this.title}) : super(key: key);
@@ -12,7 +17,31 @@ class FavPage extends StatefulWidget {
 }
 
 class _FavPageState extends State<FavPage> {
-  
+    static int _isPressed = 0;
+
+  /* Test purposes only: */
+  static final _floatingButtonState = [
+    Icon(Icons.keyboard_voice),
+    Icon(Icons.stop),
+  ];
+  static final _floatingButtonColor = [
+    MAIN_COLOR,
+    Colors.red,
+  ];
+
+  Icon _buttonIcon = _floatingButtonState[_isPressed];
+  Color _buttonColor = _floatingButtonColor[_isPressed];
+
+  void _streamAudio() {
+    setState(() {
+      _buttonIcon = _floatingButtonState[_isPressed];
+      _buttonColor = _floatingButtonColor[_isPressed];
+      _isPressed = (_isPressed + 1) % 2;
+    });
+
+    StreamController<String> controller = new StreamController();
+    //Stream<String> audioStream = new Stream.fromFuture();
+  }
 
   @override
   Widget build(BuildContext context) {
