49a2d51ee0a6858262c191ba08a5b78c98900672
Aaron Alef <aaron.alef@code.berlin>
2019-03-04 11:19:29 +0100

    removed artifacts

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,7 @@ import 'routes.dart';
   Copy of base application example provided by Google
  */
 
-//TODO: Move to corresponding class
-const int PORT = 12345;
-const String HOSTNAME = "127.0.0.1";
 const Color MAIN_COLOR = Colors.cyan;
-
 
 // Main function running the class containing the whole project
 void main () {
