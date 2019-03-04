cec6b7f9e6ae98b396eeb9d6fadabb3af74ffe1b
Aaron Alef <aaron.alef@code.berlin>
2019-03-04 11:18:52 +0100

    Removed unneeded dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:notepadai_app/routes/homescreen.dart';
 import 'dart:async';
 import 'package:notepadai_app/design elements/DiamondBorder.dart';
-import 'package:audioplayers/audioplayers.dart';
-import 'package:audio_recorder/audio_recorder.dart';
-import 'package:notepadai_app/proto/audioStream.pbgrpc.dart';
-import 'package:notepadai_app/proto/audioStream.pb.dart';
-import 'package:grpc/grpc.dart';
 
 /* NotepadAI
   Copy of base application example provided by Google
