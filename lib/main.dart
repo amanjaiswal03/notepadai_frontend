2fc2eecb6b8d08cf51aca2652f2b029723ba15ac
Aaron Alef <aaron.alef@code.berlin>
2019-03-11 20:02:05 +0100

    Removed old artifacts

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
-import 'package:audio_recorder/audio_recorder.dart';
-import 'package:notepadai_app/proto/audioStream.pbgrpc.dart';
-import 'package:notepadai_app/proto/audioStream.pb.dart';
-import 'package:grpc/grpc.dart';
 
 /* NotepadAI
   Copy of base application example provided by Google
  */
-
-//TODO: Move to corresponding class
-const int PORT = 12345;
-const String HOSTNAME = "127.0.0.1";
-
 
 // Main function running the class containing the whole project
 void main() => runApp(NotepadAI());
