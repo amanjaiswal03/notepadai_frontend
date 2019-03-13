d3d6d598aed7af12138196916e0fe274ef62e94c
Aaron Alef <aaron.alef@code.berlin>
2019-03-13 10:54:17 +0100

    Undo merge

--- lib/src/generated/audioStream.pbgrpc.dart
+++ lib/src/generated/audioStream.pbgrpc.dart
@@ -6,21 +6,20 @@
 
 import 'dart:async' as $async;
 
-import 'package:grpc/grpc.dart' as $grpc;
+import 'package:grpc/service_api.dart' as $grpc;
 import 'audioStream.pb.dart';
 export 'audioStream.pb.dart';
 
 class AudioProcessorClient extends $grpc.Client {
-  static final _$transcriptAudio = new $grpc.ClientMethod<AudioChunk, Sentence>(
-      '/AudioProcessor/TranscriptAudio',
-      (AudioChunk value) => value.writeToBuffer(),
-      (List<int> value) => new Sentence.fromBuffer(value));
+  static final _$transcriptAudio = new $grpc.ClientMethod<Samples, Response>(
+      '/AudioProcessor/transcriptAudio',
+      (Samples value) => value.writeToBuffer(),
+      (List<int> value) => new Response.fromBuffer(value));
 
   AudioProcessorClient($grpc.ClientChannel channel, {$grpc.CallOptions options})
       : super(channel, options: options);
 
-  $grpc.ResponseStream<Sentence> transcriptAudio(
-      $async.Stream<AudioChunk> request,
+  $grpc.ResponseStream<Response> transcriptAudio($async.Stream<Samples> request,
       {$grpc.CallOptions options}) {
     final call = $createCall(_$transcriptAudio, request, options: options);
     return new $grpc.ResponseStream(call);
@@ -31,15 +30,15 @@ abstract class AudioProcessorServiceBase extends $grpc.Service {
   String get $name => 'AudioProcessor';
 
   AudioProcessorServiceBase() {
-    $addMethod(new $grpc.ServiceMethod<AudioChunk, Sentence>(
-        'TranscriptAudio',
+    $addMethod(new $grpc.ServiceMethod<Samples, Response>(
+        'transcriptAudio',
         transcriptAudio,
         true,
         true,
-        (List<int> value) => new AudioChunk.fromBuffer(value),
-        (Sentence value) => value.writeToBuffer()));
+        (List<int> value) => new Samples.fromBuffer(value),
+        (Response value) => value.writeToBuffer()));
   }
 
-  $async.Stream<Sentence> transcriptAudio(
-      $grpc.ServiceCall call, $async.Stream<AudioChunk> request);
+  $async.Stream<Response> transcriptAudio(
+      $grpc.ServiceCall call, $async.Stream<Samples> request);
 }
