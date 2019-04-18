2dc52ee4f088402011236c82e386e7e5ab36af51
Aaron Alef <aaron.alef@code.berlin>
2019-04-18 14:19:00 +0200

    Support not possible

--- android/app/src/main/kotlin/com/code/aaron/notepadaiapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/code/aaron/notepadaiapp/MainActivity.kt
@@ -3,11 +3,12 @@ package com.code.aaron.notepadaiapp
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
+
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
 }
